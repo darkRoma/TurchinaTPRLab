4baad970854b5b1f5cb2136a8b865b6078210125
Artyom Gavryushov <lourfinus@yahoo.com>
2014-10-14 02:54:44 +0300

    added application frame

---
+++ TurchinaTPRLab/NonRandomizedSolutionsForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TurchinaTPRLab
+{
+    public partial class NonRandomizedSolutionsForm : Form
+    {
+        public NonRandomizedSolutionsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void NonRandomizedSolutionsForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void miniMaxCriterionPictureBox_Click(object sender, EventArgs e)
+        {
+            MiniMaxCriterionInfoForm miniMaxCriterionInfoForm = new MiniMaxCriterionInfoForm();
+            miniMaxCriterionInfoForm.Show();
+        }
+
+        private void savageCriterionPictureBox_Click(object sender, EventArgs e)
+        {
+            SavageCriterionInfoForm savageCriterionInfoForm = new SavageCriterionInfoForm();
+            savageCriterionInfoForm.Show();
+        }
+
+        private void bayesianCriterionPictureBox_Click(object sender, EventArgs e)
+        {
+            BayesianCriterionInfoForm bayesianCriterionInfoForm = new BayesianCriterionInfoForm();
+            bayesianCriterionInfoForm.Show();
+        }
+
+        private void hurwitzCriterionPictureBox_Click(object sender, EventArgs e)
+        {
+            HurwitzCriterionInfoForm hurwitzCriterionInfoForm = new HurwitzCriterionInfoForm();
+            hurwitzCriterionInfoForm.Show();
+        }
+
+        private void neymanPearsonCriterionPictureBox_Click(object sender, EventArgs e)
+        {
+            NeymanPearsonCriterionInfoForm neymanPearsonCriterionInfoForm = new NeymanPearsonCriterionInfoForm();
+            neymanPearsonCriterionInfoForm.Show();
+        }
+    }
+}
